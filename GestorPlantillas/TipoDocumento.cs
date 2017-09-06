9c45d680f379f7935450d21d088cb4cc72d286ab
cursoCiC <cursoCiC@email>
2017-09-06 18:29:07 +0200

    Añadidos campos al TipoDocumento

--- GestorPlantillas/TipoDocumento.cs
+++ GestorPlantillas/TipoDocumento.cs
@@ -11,5 +11,8 @@ namespace GestorPlantillas
 		public String Descripcion { get; set; }
 		public String Mime { get; set; }
 		public String Extension { get; set; }
+		public bool Estructurado { get; set; }
+		public bool Editable { get; set; }
+		public float Tamanio { get; set; }
 	}
 }
